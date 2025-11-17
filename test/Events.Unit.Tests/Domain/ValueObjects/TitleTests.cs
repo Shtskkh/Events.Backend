a05f2b822186393e2ea3aa39d38fa70d6b8979c8
Alexey Shatskikh <shtskkh@gmail.com>
2025-11-17 23:04:15 +0700

    [dev/domain] TitleTests.cs тесты переделаны на FluentAssertions.

--- test/Events.Unit.Tests/Domain/ValueObjects/TitleTests.cs
+++ test/Events.Unit.Tests/Domain/ValueObjects/TitleTests.cs
@@ -1,4 +1,5 @@
 using Events.Domain.ValueObjects;
+using FluentAssertions;
 
 namespace Events.Unit.Tests.Domain.ValueObjects;
 
@@ -7,46 +8,44 @@ public class TitleTests
 	[Fact]
 	public void Constructor_ValidTitle_DoesNotThrow()
 	{
-		var str1 = "Hello World";
+		const string str1 = "Hello World";
 		var title1 = new Title(str1);
 
-		var str2 = "Hello World ";
+		const string str2 = " Hello World ";
 		var title2 = new Title(str2);
 
-		Assert.Equal(str1, title1.Value);
-		Assert.Equal(title2.Value, title2.Value);
+		title1.Value.Should().Be(str1);
+		title2.Value.Should().Be(str2.Trim());
+		title1.Value.Should().Be(title1.Value);
 	}
 
 	[Fact]
 	public void Constructor_WithNullValue_ThrowsArgumentException()
 	{
-		var ex = Assert.Throws<ArgumentException>(() => new Title(null));
+		var createTitle = () => new Title(null);
 
-		Assert.Equal(
-			"Название не может быть null или пустым.",
-			ex.Message
-		);
+		createTitle.Should()
+			.Throw<ArgumentException>()
+			.WithMessage("Название не может быть null или пустым.");
 	}
 
 	[Fact]
 	public void Constructor_WithEmptyTitle_ThrowsArgumentException()
 	{
-		var ex = Assert.Throws<ArgumentException>(() => new Title(""));
+		var createTitle = () => new Title(string.Empty);
 
-		Assert.Equal(
-			"Название не может быть null или пустым.",
-			ex.Message
-		);
+		createTitle.Should()
+			.Throw<ArgumentException>()
+			.WithMessage("Название не может быть null или пустым.");
 	}
 
 	[Fact]
 	public void Constructor_WithWhitespaceOnlyTitle_ThrowsArgumentException()
 	{
-		var ex = Assert.Throws<ArgumentException>(() => new Title("  "));
+		var createTitle = () => new Title(" ");
 
-		Assert.Equal(
-			"Название не может быть null или пустым.",
-			ex.Message
-		);
+		createTitle.Should()
+			.Throw<ArgumentException>()
+			.WithMessage("Название не может быть null или пустым.");
 	}
 }
