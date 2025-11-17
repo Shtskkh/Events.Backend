947dbed3fa4938251840054cd29f6a390aa3fd2a
Alexey Shatskikh <shtskkh@gmail.com>
2025-11-17 21:56:07 +0700

    [dev/Domain] Добавлен абстрактный класс для Value Objects

---
+++ src/Domain/Events.Domain/Shared/ValueObject.cs
@@ -0,0 +1,34 @@
+namespace Events.Domain.Shared;
+
+public abstract class ValueObject
+{
+	protected abstract IEnumerable<object> GetEqualityComponents();
+
+	public override bool Equals(object? obj)
+	{
+		if (obj == null || obj.GetType() != GetType()) return false;
+		if (ReferenceEquals(this, obj)) return true;
+
+		var other = (ValueObject)obj;
+
+		return GetEqualityComponents()
+			.SequenceEqual(other.GetEqualityComponents());
+	}
+
+	public override int GetHashCode()
+	{
+		return GetEqualityComponents()
+			.Select(component => component != null ? component.GetHashCode() : 0)
+			.Aggregate((x, y) => x ^ y);
+	}
+
+	public static bool operator ==(ValueObject? left, ValueObject? right)
+	{
+		return Equals(left, right);
+	}
+
+	public static bool operator !=(ValueObject? left, ValueObject? right)
+	{
+		return !Equals(left, right);
+	}
+}
