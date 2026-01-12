8e68c56433291e3bd8c515e56687e9162decbacf
Alexey Shatskikh <shtskkh@gmail.com>
2026-01-12 18:26:12 +0700

    [test] - Добавлен builder для тестов Event.

---
+++ test/Events.Unit.Tests/Domain/Aggregates/EventAggregate/Helpers/EventTestBuilder.cs
@@ -0,0 +1,23 @@
+﻿using Events.Domain.Aggregates.EventAggregate;
+
+namespace Events.Unit.Tests.Domain.Aggregates.EventAggregate.Helpers;
+
+public class EventTestBuilder
+{
+    private readonly Guid _id = Guid.NewGuid();
+    private string _title = "Test title";
+
+    public Event Build()
+    {
+        return new Event(
+            _id,
+            _title
+        );
+    }
+
+    public EventTestBuilder WithTitle(string title)
+    {
+        _title = title;
+        return this;
+    }
+}
