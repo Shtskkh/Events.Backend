471d498454d53f3bdd18fd04fc7c03e9b3a8bb91
Alexey Shatskikh <shtskkh@gmail.com>
2026-01-12 16:29:39 +0700

    [domain] - Добавлен класс для доменных исключений.

---
+++ src/Domain/Events.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,12 @@
+﻿namespace Events.Domain.Exceptions;
+
+public class DomainException : Exception
+{
+    public DomainException()
+    {
+    }
+
+    public DomainException(string message) : base(message)
+    {
+    }
+}
