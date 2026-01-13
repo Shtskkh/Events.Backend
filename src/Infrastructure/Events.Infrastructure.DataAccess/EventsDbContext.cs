ac58e8a805ab98dd071527bdee83ebb7a5432c24
Alexey Shatskikh <shtskkh@gmail.com>
2026-01-13 20:42:50 +0700

    [infrastructure] - Добавлен контекст базы данных.

---
+++ src/Infrastructure/Events.Infrastructure.DataAccess/EventsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace Events.Infrastructure.DataAccess;
+
+public class EventsDbContext : DbContext
+{
+    public EventsDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
