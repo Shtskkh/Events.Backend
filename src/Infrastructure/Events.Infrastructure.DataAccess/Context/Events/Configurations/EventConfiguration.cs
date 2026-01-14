5855673e4c87522cec727b963c5e7f82e5ebb411
Alexey Shatskikh <shtskkh@gmail.com>
2026-01-14 14:41:51 +0700

    [infrastructure] - Добавлена конфигрурация для Event.

---
+++ src/Infrastructure/Events.Infrastructure.DataAccess/Context/Events/Configurations/EventConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Events.Domain.Aggregates.EventAggregate;
+using Events.Domain.Aggregates.EventAggregate.ValueObjects;
+using Events.Domain.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Events.Infrastructure.DataAccess.Context.Events.Configurations;
+
+public class EventConfiguration : IEntityTypeConfiguration<Event>
+{
+    public void Configure(EntityTypeBuilder<Event> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.Title)
+            .HasConversion(
+                v => v.Value,
+                v => new EventTitle(v)
+            )
+            .HasMaxLength(DomainConstraints.Event.EventTitle.MaxLength)
+            .IsRequired();
+    }
+}
