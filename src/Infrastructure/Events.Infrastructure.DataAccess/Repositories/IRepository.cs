cd36aab95cb9fa3f9d52293599f4828d694a1004
Alexey Shatskikh <shtskkh@gmail.com>
2026-01-20 18:10:23 +0700

    [infrastructure] - Добавлен интерфейс глупого репозитория.

---
+++ src/Infrastructure/Events.Infrastructure.DataAccess/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using Events.Domain.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace Events.Infrastructure.DataAccess.Repositories;
+
+public interface IRepository<TEntity, in TKey, TContext>
+    where TEntity : Entity<TKey>
+    where TKey : IEquatable<TKey>
+    where TContext : DbContext
+{
+    IQueryable<TEntity> GetAllAsync();
+    Task<TEntity> GetByIdAsync(TKey id);
+    Task<bool> IsExistsAsync(TKey id);
+    Task AddAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task DeleteAsync(TKey id);
+}
