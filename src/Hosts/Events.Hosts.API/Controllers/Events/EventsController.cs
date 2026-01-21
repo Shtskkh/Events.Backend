0d60b0e35ed27790e4dfec99f6ca4287c7e39fa3
Alexey Shatskikh <shtskkh@gmail.com>
2026-01-21 20:33:47 +0700

    [hosts] - Начало реализации контроллера Events.

---
+++ src/Hosts/Events.Hosts.API/Controllers/Events/EventsController.cs
@@ -0,0 +1,40 @@
+﻿using Events.Domain.Aggregates.EventAggregate;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Events.Hosts.API.Controllers.Events;
+
+[ApiController]
+[Route("api/v/1/[controller]")]
+public class EventsController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(ICollection<Event>), StatusCodes.Status200OK, "application/json")]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPatch("{id:guid}")]
+    public async Task<IActionResult> UpdateAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
