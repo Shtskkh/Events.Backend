3cf5062a057287281f7f95507278eba91d627a4d
Alexey Shatskikh <shtskkh@gmail.com>
2026-01-22 15:56:08 +0700

    [contracts] - Добавлено DTO для краткого описания мероприятия.

---
+++ src/Contracts/Events.Contracts/Features/Events/DTOs/ShortEventDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace Events.Contracts.Features.Events.DTOs;
+
+/// <summary>
+///     Краткая информация о мероприятии.
+/// </summary>
+public class ShortEventDto
+{
+    /// <summary>
+    ///     Название мероприятия.
+    /// </summary>
+    public required string Title { get; set; }
+
+    /// <summary>
+    ///     Анонс (краткое описание) мероприятия.
+    /// </summary>
+    public required string Announcement { get; set; }
+
+    /// <summary>
+    ///     Описание мероприятия.
+    /// </summary>
+    public required string Description { get; set; }
+}
