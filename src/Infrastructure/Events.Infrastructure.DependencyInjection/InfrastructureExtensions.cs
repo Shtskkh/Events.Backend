223dde0005c53732ff96ae558e2a090b5983d228
Alexey Shatskikh <shtskkh@gmail.com>
2026-01-22 14:35:32 +0700

    [hosts/infrastructure] - Добавлена конфигурация для подключения к БД.

--- src/Infrastructure/Events.Infrastructure.DependencyInjection/InfrastructureExtensions.cs
+++ src/Infrastructure/Events.Infrastructure.DependencyInjection/InfrastructureExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Events.Infrastructure.DataAccess.Repositories;
+﻿using Events.Infrastructure.DataAccess;
+using Events.Infrastructure.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Events.Infrastructure.DependencyInjection;
@@ -7,9 +10,18 @@ public static class InfrastructureExtensions
 {
     extension(IServiceCollection services)
     {
-        public void AddInfrastructure()
+        public void AddInfrastructure(IConfiguration configuration)
         {
+            services.ConfigureDbConnection(configuration);
+
             services.AddScoped(typeof(IRepository<,,>), typeof(Repository<,,>));
+        }
+
+        private void ConfigureDbConnection(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DbConnection");
+            services.AddDbContextPool<EventsDbContext>(options => options.UseNpgsql(connectionString,
+                optionBuilder => { optionBuilder.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery); }));
         }
     }
 }
