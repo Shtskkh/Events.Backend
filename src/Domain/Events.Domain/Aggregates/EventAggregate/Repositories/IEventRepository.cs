5747d9bfd1af6233b573a91d6837026e84f2e176
Alexey Shatskikh <shtskkh@gmail.com>
2026-01-22 15:59:51 +0700

    [domain] - Добавлен интерфейс репозитория IEventRepository.

---
+++ src/Domain/Events.Domain/Aggregates/EventAggregate/Repositories/IEventRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace Events.Domain.Aggregates.EventAggregate.Repositories;
+
+/// <summary>
+///     Репозиторий мероприятий.
+/// </summary>
+public interface IEventRepository
+{
+    /// <summary>
+    ///     Получить мероприятие по ID.
+    /// </summary>
+    /// <param name="id">ID мероприятия.</param>
+    /// <returns>
+    ///     Сущность мероприятия.
+    /// </returns>
+    Task<Event> GetByIdAsync(Guid id);
+}
