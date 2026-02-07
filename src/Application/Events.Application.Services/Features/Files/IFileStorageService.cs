cf0cc6fb0ed80a54be59a6e82609cad4382c694d
Alexey Shatskikh <shtskkh@gmail.com>
2026-02-07 16:56:55 +0700

    Добавлен интерфейс для S3 хранилищ.

---
+++ src/Application/Events.Application.Services/Features/Files/IFileStorageService.cs
@@ -0,0 +1,15 @@
+﻿using Amazon.S3.Model;
+
+namespace Events.Application.Services.Features.Files;
+
+/// <summary>
+///     Интерфейс для файловых сервисов.
+/// </summary>
+public interface IFileStorageService
+{
+    /// <summary>
+    ///     Загрузить файл в хранилище.
+    /// </summary>
+    /// <param name="request">Запрос на добавление.</param>
+    Task PutObjectAsync(PutObjectRequest request);
+}
