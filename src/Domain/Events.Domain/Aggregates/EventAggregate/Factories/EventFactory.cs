1c795487fd101cea438cd06b084a4829aedce50b
Alexey Shatskikh <shtskkh@gmail.com>
2026-02-07 22:46:27 +0700

    Добавлена фабрика создания мероприятия.

---
+++ src/Domain/Events.Domain/Aggregates/EventAggregate/Factories/EventFactory.cs
@@ -0,0 +1,27 @@
+﻿using Events.Domain.Aggregates.EventAggregate.ValueObjects;
+
+namespace Events.Domain.Aggregates.EventAggregate.Factories;
+
+/// <inheritdoc />
+public class EventFactory : IEventFactory
+{
+    /// <inheritdoc />
+    public Event Create(string title, string announcement, string description, DateTimeOffset startDateTime,
+        DateTimeOffset endDateTime, Guid? previewFilename)
+    {
+        var id = Guid.NewGuid();
+        var titleVo = new EventTitle(title);
+        var announcementVo = new EventAnnouncement(announcement);
+        var descriptionVo = new EventDescription(description);
+
+        return new Event(
+            id,
+            titleVo,
+            announcementVo,
+            descriptionVo,
+            startDateTime,
+            endDateTime,
+            previewFilename
+        );
+    }
+}
---
+++ src/Domain/Events.Domain/Aggregates/EventAggregate/Factories/IEventFactory.cs
@@ -0,0 +1,26 @@
+﻿namespace Events.Domain.Aggregates.EventAggregate.Factories;
+
+/// <summary>
+///     Фабрика создания мероприятия.
+/// </summary>
+public interface IEventFactory
+{
+    /// <summary>
+    ///     Создание нового мероприятия.
+    /// </summary>
+    /// <param name="title">Название мероприятие.</param>
+    /// <param name="announcement">Анонс мероприятия.</param>
+    /// <param name="description">Описание мероприятия.</param>
+    /// <param name="startDateTime">Дата и время начала мероприятия.</param>
+    /// <param name="endDateTime">Дата и время окончания мероприятия.</param>
+    /// <param name="previewFilename">Название файла превью.</param>
+    /// <returns>Объект созданного мероприятия.</returns>
+    Event Create(
+        string title,
+        string announcement,
+        string description,
+        DateTimeOffset startDateTime,
+        DateTimeOffset endDateTime,
+        Guid? previewFilename
+    );
+}
