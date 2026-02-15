9aac9e287d8a76f4cabf7ce4372b9bd9b8a380d7
Alexey Shatskikh <shtskkh@gmail.com>
2026-02-15 16:22:26 +0700

    Добавлен интерфейс аудита.

---
+++ src/Domain/Events.Domain/Shared/Interfaces/IAuditable.cs
@@ -0,0 +1,17 @@
+﻿namespace Events.Domain.Shared.Interfaces;
+
+/// <summary>
+///     Интерфейс аудита.
+/// </summary>
+public interface IAuditable
+{
+    /// <summary>
+    ///     Дата создания.
+    /// </summary>
+    DateTime CreatedAt { get; }
+
+    /// <summary>
+    ///     Дата обновления.
+    /// </summary>
+    DateTime UpdatedAt { get; }
+}
