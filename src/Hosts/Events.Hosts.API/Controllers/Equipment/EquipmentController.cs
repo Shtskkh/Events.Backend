6ca56dcc351aedbf9b39c9ca5f920643513dc814
Alexey Shatskikh <shtskkh@gmail.com>
2026-02-24 11:44:14 +0700

    Начало работы над контроллером оборудования.

---
+++ src/Hosts/Events.Hosts.API/Controllers/Equipment/EquipmentController.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Events.Hosts.API.Controllers.Equipment;
+
+[ApiController]
+[Route("/api/v/1/[controller]")]
+public class EquipmentController(IMediator mediator) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetByFilterAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPatch("{id:int}")]
+    public async Task<IActionResult> UpdateAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
