ec7183c12ed6bd6738863053a86eec1d8ddbef12
Alexey Shatskikh <shtskkh@gmail.com>
2026-02-24 22:30:40 +0700

    Добавлена фабрика создания локации.

---
+++ src/Domain/Events.Domain/Aggregates/LocationAggregate/Factories/ILocationFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace Events.Domain.Aggregates.LocationAggregate.Factories;
+
+/// <summary>
+///     Фабрика локации.
+/// </summary>
+public interface ILocationFactory
+{
+    /// <summary>
+    ///     Создать локацию.
+    /// </summary>
+    /// <param name="title">Название локации.</param>
+    /// <param name="address">Адрес локации.</param>
+    /// <returns>Объект локации.</returns>
+    public Location Create(string title, string address);
+}
---
+++ src/Domain/Events.Domain/Aggregates/LocationAggregate/Factories/LocationFactory.cs
@@ -0,0 +1,16 @@
+﻿using Events.Domain.Aggregates.LocationAggregate.ValueObjects;
+
+namespace Events.Domain.Aggregates.LocationAggregate.Factories;
+
+/// <inheritdoc />
+public class LocationFactory : ILocationFactory
+{
+    /// <inheritdoc />
+    public Location Create(string title, string address)
+    {
+        var titleVo = new LocationTitle(title);
+        var addressVo = new LocationAddress(address);
+
+        return new Location(0, titleVo, addressVo);
+    }
+}
