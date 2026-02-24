2c44224a858c70ef3b6690098463a366b0cb7428
Alexey Shatskikh <shtskkh@gmail.com>
2026-02-24 10:29:18 +0700

    Начало работы над контроллером пользователей.

---
+++ src/Hosts/Events.Hosts.API/Controllers/Users/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using Events.Contracts.Errors;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Events.Hosts.API.Controllers.Users;
+
+/// <summary>
+///     Контроллер пользователей.
+/// </summary>
+/// <param name="mediator">Медиатор.</param>
+[ApiController]
+[Route("api/v/1/[controller]")]
+[ProducesResponseType(typeof(ErrorDto), StatusCodes.Status500InternalServerError, "application/problem+json",
+    Description = "Неожиданная ошибка сервера.")]
+public class UsersController(IMediator mediator) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetByFilterAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPatch("{id:guid}")]
+    public async Task<IActionResult> UpdateAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
