c22f834448c1cbae917e5a14157be2a1aa8615d1
Alexey Shatskikh <shtskkh@gmail.com>
2026-02-25 09:54:07 +0700

    Добавлен репозиторий помещений.

---
+++ src/Application/Events.Application.Services/Features/Locations/Repositories/IPlaceRepository.cs
@@ -0,0 +1,16 @@
+﻿using Events.Domain.Aggregates.LocationAggregate;
+
+namespace Events.Application.Services.Features.Locations.Repositories;
+
+/// <summary>
+///     Репозиторий помещений.
+/// </summary>
+public interface IPlaceRepository
+{
+    /// <summary>
+    ///     Добавить помещение.
+    /// </summary>
+    /// <param name="place">Объект помещения.</param>
+    /// <returns>ID добавленного помещения.</returns>
+    Task<int> AddPlaceAsync(Place place);
+}
---
+++ src/Infrastructure/Events.Infrastructure.DataAccess/Context/Locations/Repositories/PlaceRepository.cs
@@ -0,0 +1,16 @@
+﻿using Events.Application.Services.Features.Locations.Repositories;
+using Events.Domain.Aggregates.LocationAggregate;
+using Events.Infrastructure.DataAccess.Repositories;
+
+namespace Events.Infrastructure.DataAccess.Context.Locations.Repositories;
+
+/// <inheritdoc />
+public class PlaceRepository(IRepository<Place, int, EventsDbContext> repository) : IPlaceRepository
+{
+    /// <inheritdoc />
+    public async Task<int> AddPlaceAsync(Place place)
+    {
+        await repository.AddAsync(place);
+        return place.Id;
+    }
+}
