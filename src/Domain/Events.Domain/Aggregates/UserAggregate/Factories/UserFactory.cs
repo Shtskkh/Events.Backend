3d3abe070bc264d7a8cbe611c3cfcce533c652d4
Alexey Shatskikh <shtskkh@gmail.com>
2026-03-10 11:37:39 +0700

    Добавлена фабрика создания пользователя.

---
+++ src/Domain/Events.Domain/Aggregates/UserAggregate/Factories/UserFactory.cs
@@ -0,0 +1,37 @@
+﻿using Events.Domain.Aggregates.UserAggregate.ValueObjects;
+using Events.Domain.Shared.ValueObjects;
+
+namespace Events.Domain.Aggregates.UserAggregate.Factories;
+
+/// <summary>
+///     Фабрика пользователя.
+/// </summary>
+public static class UserFactory
+{
+    /// <summary>
+    /// </summary>
+    /// <param name="lastName">Фамилия.</param>
+    /// <param name="firstName">Имя.</param>
+    /// <param name="email">Почтовый адрес.</param>
+    /// <param name="password">Пароль.</param>
+    /// <param name="role">Роль.</param>
+    /// <param name="avatarFilename">Название файла аватара.</param>
+    /// <param name="patronymic">Отчество</param>
+    /// <returns>Объект сущности пользователя.</returns>
+    public static User Create(
+        string lastName,
+        string firstName,
+        string email,
+        string password,
+        UserRole role,
+        string? avatarFilename = null,
+        string? patronymic = null)
+    {
+        var id = Guid.NewGuid();
+        var personNameVo = new PersonName(lastName, firstName, patronymic);
+        var emailVo = new Email(email);
+        var passwordVo = new Password(password);
+
+        return new User(id, personNameVo, emailVo, passwordVo, role, avatarFilename);
+    }
+}
