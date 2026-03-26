f86028dad09ee63dbecfd5881107a39aaa117473
Alexey Shatskikh <shtskkh@gmail.com>
2026-03-26 15:38:59 +0700

    Фикс маппинга Place.

--- src/Application/Events.Application.Services/Features/Locations/Mappings/PlaceProfile.cs
+++ src/Application/Events.Application.Services/Features/Locations/Mappings/PlaceProfile.cs
@@ -13,13 +13,15 @@ public class PlaceProfile : Profile
         CreateMap<Place, ShortPlaceDto>()
             .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
             .ForMember(dest => dest.Number, opt => opt.MapFrom(src => src.Number.Value))
+            .ForMember(dest => dest.Capacity, opt => opt.MapFrom(src => src.Capacity.Value))
             .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.Type.Title))
-            .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title ?? null));
+            .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title.Value));
 
         CreateMap<Place, PlaceDto>()
             .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
             .ForMember(dest => dest.Number, opt => opt.MapFrom(src => src.Number.Value))
+            .ForMember(dest => dest.Capacity, opt => opt.MapFrom(src => src.Capacity.Value))
             .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.Type.Title))
-            .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title ?? null));
+            .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title.Value));
     }
 }
