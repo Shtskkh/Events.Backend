13d84e5a261e2e221e14f3f34b77bcd5d5d2fd22
Alexey Shatskikh <shtskkh@gmail.com>
2026-03-30 17:58:02 +0700

    Добавлена регистрация репозитория для типов оборудования.

--- src/Infrastructure/Events.Infrastructure.DataAccess/DataAccessExtensions.cs
+++ src/Infrastructure/Events.Infrastructure.DataAccess/DataAccessExtensions.cs
@@ -1,9 +1,11 @@
 ﻿using Amazon.Runtime;
 using Amazon.S3;
+using Events.Application.Services.Features.Equipment.Repositories;
 using Events.Application.Services.Features.Events.Repositories;
 using Events.Application.Services.Features.Files;
 using Events.Application.Services.Features.Locations.Repositories;
 using Events.Application.Services.Features.Users.Repositories;
+using Events.Infrastructure.DataAccess.Context.Equipment.Repositories;
 using Events.Infrastructure.DataAccess.Context.Events.Repositories;
 using Events.Infrastructure.DataAccess.Context.Locations.Repositories;
 using Events.Infrastructure.DataAccess.Context.Users.Repositories;
@@ -62,6 +64,8 @@ public static class DataAccessExtensions
 
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IUserRoleRepository, UserRoleRepository>();
+
+            services.AddScoped<IEquipmentTypeRepository, EquipmentTypeRepository>();
         }
 
         private void AddS3Client(IConfiguration configuration)
