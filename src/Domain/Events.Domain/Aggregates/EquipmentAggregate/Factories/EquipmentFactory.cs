ba74ba8af83023636ee67b25c0ce7b374e1a61ff
Alexey Shatskikh <shtskkh@gmail.com>
2026-03-31 11:16:54 +0700

    Добавлена фабрика создания оборудования.

---
+++ src/Domain/Events.Domain/Aggregates/EquipmentAggregate/Factories/EquipmentFactory.cs
@@ -0,0 +1,16 @@
+﻿using Events.Domain.Aggregates.EquipmentAggregate.ValueObjects;
+
+namespace Events.Domain.Aggregates.EquipmentAggregate.Factories;
+
+/// <summary>
+///     Фабрика оборудования.
+/// </summary>
+public static class EquipmentFactory
+{
+    public static Equipment Create(string title, EquipmentType type, int? placeId = null)
+    {
+        var titleVo = new EquipmentTitle(title);
+
+        return new Equipment(default, titleVo, type, placeId);
+    }
+}
