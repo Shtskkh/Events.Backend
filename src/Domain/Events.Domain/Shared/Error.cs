3bec6675914e84287241a10bf4dc3e436a418e1b
Alexey Shatskikh <shtskkh@gmail.com>
2026-04-15 14:55:14 +0700

    Добавлен record для ошибок.

---
+++ src/Domain/Events.Domain/Shared/Error.cs
@@ -0,0 +1,8 @@
+﻿namespace Events.Domain.Shared;
+
+/// <summary>
+///     Record для ошибок.
+/// </summary>
+/// <param name="ErrorCode">Код ошибки.</param>
+/// <param name="ErrorMessage">Сообщение ошибки.</param>
+public record Error(string ErrorCode, string ErrorMessage);
