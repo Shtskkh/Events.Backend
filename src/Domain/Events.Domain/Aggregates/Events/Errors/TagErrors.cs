1943b9dd1082336b887c0a179e5973e2cf8fc422
Alexey Shatskikh <shtskkh@gmail.com>
2026-04-17 14:03:49 +0700

    Добавлена сущность тэга и его ошибки.

---
+++ src/Domain/Events.Domain/Aggregates/Events/Errors/TagErrors.cs
@@ -0,0 +1,11 @@
+﻿using Events.Domain.Shared;
+
+namespace Events.Domain.Aggregates.Events.Errors;
+
+public static class TagErrors
+{
+    public static Error ContainsWhiteSpace => new("Tag.ContainsWhiteSpace", "Тэг не может содержать пробелов.");
+
+    public static Error GreaterThanMaxLength =>
+        new("Tag.GreaterThanMaxLength", $"Тэг больше максимальной длинны в {Tag.MaxLength}");
+}
