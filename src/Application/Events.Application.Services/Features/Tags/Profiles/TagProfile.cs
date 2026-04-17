a350991d145aceaaf52c79b0b53cf743f13d0c0d
Alexey Shatskikh <shtskkh@gmail.com>
2026-04-17 14:53:26 +0700

    Добавлен профиль маппинга для тэгов.

---
+++ src/Application/Events.Application.Services/Features/Tags/Profiles/TagProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Events.Contracts.Tags;
+using Events.Domain.Aggregates.Events;
+
+namespace Events.Application.Services.Features.Tags.Profiles;
+
+public sealed class TagProfile : Profile
+{
+    public TagProfile()
+    {
+        CreateMap<Tag, TagDto>();
+    }
+}
---
+++ src/Contracts/Events.Contracts/Tags/TagDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Events.Contracts.Tags;
+
+/// <summary>
+///     Модель тэга.
+/// </summary>
+public sealed record TagDto
+{
+    /// <summary>
+    ///     ID.
+    /// </summary>
+    public int Id { get; init; }
+
+    /// <summary>
+    ///     Тэг.
+    /// </summary>
+    public int Value { get; init; }
+}
